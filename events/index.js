ac1a9bff8ee34f86a7e3544eed78cc3473a8550b
liyikunl@live.com <liyikunl@live.com>
2019-08-19 01:01:55 +0800

    event

---
+++ events/index.js
@@ -0,0 +1,23 @@
+const EventEmitter = require('events');
+
+
+const emitter = new EventEmitter()
+
+emitter.on("event", function (message) {
+    console.log(message)
+})
+
+emitter.emit("event", "I am message!")
+
+
+var proxy = new EventEmitter()
+
+let callback = () => console.log("abc")
+let callback2 = () => console.log("abc2")
+
+proxy.once("selected",callback)
+
+
+proxy.once("selected",callback2)
+
+proxy.emit("selected")
